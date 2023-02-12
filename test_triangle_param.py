3c57fdc97184eb4b345492951abaa70c039793fa
NikitaMogilev <nikitamogilev0@gmail.com>
2023-02-12 23:07:21 +0300

    инит

--- test_triangle_param.py
+++ test_triangle_param.py
@@ -1,11 +1,11 @@
 from triangle.triangle import Triangle
-import pytest
+import lesson_py_test
 
 
 class TestTrianglePtest:
 
-    @pytest.mark.parametrize("a, b, c",
-                             [(7, 8, 9), (9, 7, 8)])
+    @lesson_py_test.mark.parametrize("a, b, c",
+                                     [(7, 8, 9), (9, 7, 8)])
     def test_triangle_eq(self, a, b, c):
         first = Triangle(a, b, c)
         second = Triangle(9, 8, 7)
@@ -22,8 +22,8 @@ class TestTrianglePtest:
     def test_triangle_rb(self, rb_triangle):
         assert rb_triangle.two_sides_eq()
 
-    @pytest.mark.parametrize("a, b, c",
-                             [(7, 8, 9), (9, 7, 8)])
+    @lesson_py_test.mark.parametrize("a, b, c",
+                                     [(7, 8, 9), (9, 7, 8)])
     def test_triangle_rb_2(self, rb_triangle, a, b, c):
         assert rb_triangle.two_sides_eq()
         first = Triangle(a, b, c)
