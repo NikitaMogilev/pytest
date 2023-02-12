ab065d5d86c43dbcdb3d681fa34aaad4467cbd59
NikitaMogilev <nikitamogilev0@gmail.com>
2023-02-12 20:19:47 +0300

    begin

---
+++ conftest.py
@@ -0,0 +1,23 @@
+import pytest
+from triangle.triangle import Triangle
+
+
+# @pytest.fixture(scope='class', autouse=True)
+# def printer():
+#     print("\nIt is setup\n")
+#     yield
+#     print("\nTeardown\n")
+
+
+@pytest.fixture()
+def right_triangle():
+    right_tr = Triangle(6, 6, 6)
+    yield right_tr
+    del right_tr
+
+
+@pytest.fixture(params=[(3, 3, 4), (8, 8, 10), (5, 5, 8)])
+def rb_triangle(request):
+    rb_tr = Triangle(request.param[0], request.param[1], request.param[2])
+    yield rb_tr
+    del rb_tr
---
+++ test_triangle.py
@@ -0,0 +1,16 @@
+from triangle.triangle import Triangle
+
+
+class TestTrianglePtest:
+
+    def test_triangle_eq(self, right_triangle):
+        first = right_triangle
+        assert first.is_right_triangle()
+
+    def test_triangle_lt(self):
+        first = Triangle(7, 8, 9)
+        second = Triangle(9, 10, 11)
+        assert first < second
+
+    def test_triangle_right(self, right_triangle):
+        assert right_triangle.is_right_triangle()
