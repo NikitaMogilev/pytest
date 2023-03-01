61f75a826d6a35dfd763b785c6235f3706d44bd7
NikitaMogilev <nikitamogilev0@gmail.com>
2023-03-01 22:46:20 +0300

    complete homework 22 (refresh )

--- locators.py
+++ locators.py
@@ -12,10 +12,8 @@ def test_locators(browser):
     start_title = browser.title
     locator_button_ad = (By.CSS_SELECTOR, 'a.b-btn-fleamarket__1[href="/fleamarketposting.php"]')
     button_ad = chrome.find_element(locator_button_ad[0], locator_button_ad[1])
-    locator_q_video = (By.XPATH, "//a[@href='./viewforum.php?f=286'and text()='Видеокарты']/../sup")
-    quantity_video = chrome.find_element(locator_q_video[0], locator_q_video[1])
+    locator_q_video = chrome.find_element(By.XPATH, "//a[@href='./viewforum.php?f=286'and text()='Видеокарты']/../sup")
     locator_q_dresses = (By.XPATH, "//a[@href='./viewforum.php?f=255' and text()='Платья']/../sup")
-    quantity_dresses = chrome.find_element(locator_q_dresses[0], locator_q_dresses[1])
     button_ad.click()
     finish_title = browser.title
     assert start_title != finish_title
--- test_submit.py
+++ test_submit.py
@@ -2,10 +2,12 @@ import time
 
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
+from selenium import webdriver
+from pytest import fixture
 
 
-def test_clicker(browser):
-    chrome = browser
+def test_clicker1():
+    chrome = webdriver.Chrome()
     chrome.maximize_window()
     chrome.implicitly_wait(5)
     url = 'https://ultimateqa.com/complicated-page/'
@@ -27,11 +29,9 @@ def test_clicker(browser):
     loc_answer_captcha = (By.XPATH, '//textarea[@id="et_pb_contact_message_0"]/../../div//input['
                                     '@name="et_pb_contact_captcha_0"]')
     answer_captcha_field = chrome.find_element(loc_answer_captcha[0], loc_answer_captcha[1])
-
     input_data = {'name': "Nikita", 'email': 'nikita@mail.ru', 'wrong_email': 'nikita@mail', 'message': 'Hello world',
-              'captcha': f'{captcha_answer}'}
+                  'captcha': f'{captcha_answer}'}
 
-    # INPUT1
     name_field.send_keys(f"{input_data.get('name')}")
     email_field.send_keys(f"{input_data.get('email')}")
     message_field.send_keys(f"{input_data.get('message')}")
@@ -46,7 +46,32 @@ def test_clicker(browser):
     assert result == 'Thanks for contacting us'
     chrome.refresh()
 
-    # INPUT2
+
+def test_clicker2():
+    chrome = webdriver.Chrome()
+    chrome.maximize_window()
+    chrome.implicitly_wait(5)
+    url = 'https://ultimateqa.com/complicated-page/'
+    chrome.get(url=url)
+
+    loc_name_field = (By.XPATH, '//input[@id="et_pb_contact_name_0"]')
+    name_field = chrome.find_element(loc_name_field[0], loc_name_field[1])
+
+    loc_email_field = (By.XPATH, '//input[@id="et_pb_contact_email_0"]')
+    email_field = chrome.find_element(loc_email_field[0], loc_email_field[1])
+
+    loc_message_field = (By.XPATH, '//textarea[@id="et_pb_contact_message_0"]')
+    message_field = chrome.find_element(loc_message_field[0], loc_message_field[1])
+
+    # Решение КАПЧИ
+    loc_task_captcha = (By.XPATH, '//textarea[@id="et_pb_contact_message_0"]/../../div//span')
+    result_task_captcha = chrome.find_element(loc_task_captcha[0], loc_task_captcha[1]).text
+    captcha_answer = eval(result_task_captcha)
+    loc_answer_captcha = (By.XPATH, '//textarea[@id="et_pb_contact_message_0"]/../../div//input['
+                                    '@name="et_pb_contact_captcha_0"]')
+    answer_captcha_field = chrome.find_element(loc_answer_captcha[0], loc_answer_captcha[1])
+    input_data = {'name': "Nikita", 'email': 'nikita@mail.ru', 'wrong_email': 'nikita@mail', 'message': 'Hello world',
+                  'captcha': f'{captcha_answer}'}
     loc_name_field = (By.XPATH, '//input[@id="et_pb_contact_name_0"]')
     name_field = chrome.find_element(loc_name_field[0], loc_name_field[1])
     name_field.send_keys(f"{input_data.get('name')}")
@@ -55,15 +80,37 @@ def test_clicker(browser):
     submit_but.click()
     alert_message = chrome.find_element(By.XPATH, '//*[@id="et_pb_contact_form_0"]/div[@class="et-pb-contact-message"]')
     text_alert_message = alert_message.text
-    if alert_message:
-        test1 = True
-    else:
-        test1 = False
-    assert test1 is True and text_alert_message != result
+    assert text_alert_message != 'Thanks for contacting us'
     chrome.refresh()
 
-    # INPUT3
+
+def test_clicker3():
+    chrome = webdriver.Chrome()
+    chrome.maximize_window()
+    chrome.implicitly_wait(5)
+    url = 'https://ultimateqa.com/complicated-page/'
+    chrome.get(url=url)
+
+    loc_name_field = (By.XPATH, '//input[@id="et_pb_contact_name_0"]')
+    name_field = chrome.find_element(loc_name_field[0], loc_name_field[1])
+
+    loc_email_field = (By.XPATH, '//input[@id="et_pb_contact_email_0"]')
+    email_field = chrome.find_element(loc_email_field[0], loc_email_field[1])
+
     loc_message_field = (By.XPATH, '//textarea[@id="et_pb_contact_message_0"]')
+    message_field = chrome.find_element(loc_message_field[0], loc_message_field[1])
+
+    # Решение КАПЧИ
+    loc_task_captcha = (By.XPATH, '//textarea[@id="et_pb_contact_message_0"]/../../div//span')
+    result_task_captcha = chrome.find_element(loc_task_captcha[0], loc_task_captcha[1]).text
+    captcha_answer = eval(result_task_captcha)
+    loc_answer_captcha = (By.XPATH, '//textarea[@id="et_pb_contact_message_0"]/../../div//input['
+                                    '@name="et_pb_contact_captcha_0"]')
+    answer_captcha_field = chrome.find_element(loc_answer_captcha[0], loc_answer_captcha[1])
+    loc_message_field = (By.XPATH, '//textarea[@id="et_pb_contact_message_0"]')
+
+    input_data = {'name': "Nikita", 'email': 'nikita@mail.ru', 'wrong_email': 'nikita@mail', 'message': 'Hello world',
+                  'captcha': f'{captcha_answer}'}
     message_field = chrome.find_element(loc_message_field[0], loc_message_field[1])
     message_field.send_keys(f"{input_data.get('message')}")
     loc_submit_but = (By.XPATH, '//*[@id="et_pb_contact_form_0"]//button[@type="submit"]')
@@ -71,10 +118,6 @@ def test_clicker(browser):
     submit_but.click()
     alert_message = chrome.find_element(By.XPATH, '//*[@id="et_pb_contact_form_0"]/div[@class="et-pb-contact-message"]')
     text_alert_message = alert_message.text
-    if alert_message:
-        test1 = True
-    else:
-        test1 = False
-    assert test1 is True and text_alert_message != result
+    assert text_alert_message != 'Thanks for contacting us'
 
     chrome.close()
